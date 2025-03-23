c144f23884e7ed030b2f3634f9c17ace21d7fc7c
liuximeng2 <sliu484@emory.edu>
2025-03-23 12:28:52 -0400

    updated code for proper training

--- model/module.py
+++ model/module.py
@@ -2,6 +2,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class EMA:
+    def __init__(self, beta):
+        super().__init__()
+        self.beta = beta
+        self.step = 0
+
+    def update_model_average(self, ma_model, current_model):
+        for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
+            old_weight, up_weight = ma_params.data, current_params.data
+            ma_params.data = self.update_average(old_weight, up_weight)
+
+    def update_average(self, old, new):
+        if old is None:
+            return new
+        return old * self.beta + (1 - self.beta) * new
+
+    def step_ema(self, ema_model, model, step_start_ema=2000):
+        if self.step < step_start_ema:
+            self.reset_parameters(ema_model, model)
+            self.step += 1
+            return
+        self.update_model_average(ema_model, model)
+        self.step += 1
+
+    def reset_parameters(self, ema_model, model):
+        ema_model.load_state_dict(model.state_dict())
+
 def render_image_size(input_size, kernel_size, stride, padding):
     output_size = ((input_size - kernel_size + 2 * padding) // stride) + 1
     return output_size
@@ -10,7 +37,7 @@ class SelfAttentionBlock(nn.Module):
     def __init__(self, channels):
         super(SelfAttentionBlock, self).__init__()
         self.channels = channels
-        self.mha = nn.MultiheadAttention(channels, num_heads=4)
+        self.mha = nn.MultiheadAttention(channels, num_heads=4, batch_first=True)
         self.ln = nn.LayerNorm([channels])
         self.ff_self = nn.Sequential(
             nn.LayerNorm([channels]),
@@ -34,38 +61,25 @@ class DoubleConv(nn.Module):
         super(DoubleConv, self).__init__()
 
         self.residule = residule
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding)
-        self.norm1 = nn.BatchNorm2d(out_channels)
-        self.norm2 = nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.norm1 = nn.GroupNorm(1, out_channels)
+        self.norm2 = nn.GroupNorm(1, out_channels)
         
-    def forward(self, x, residule=None):
-        x = self.norm1(F.gelu(self.conv1(x)))
-        x = self.norm2(self.conv2(x))
+    def forward(self, x):
+        x1 = F.gelu(self.norm1(self.conv1(x)))
+        x1 = self.norm2(self.conv2(x1))
         if self.residule:
-            return F.gelu(x + residule)
+            return F.gelu(x1 + x)
         else:
-            return x
+            return x1
     
 class DownSampleBlock(nn.Module):
     def __init__(self, in_channels, out_channels, pool_kernal_size=2, kernel_size=3, stride=1, padding=1, time_dim=256):
         super(DownSampleBlock, self).__init__()
 
-        self.process1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
-            nn.BatchNorm2d(out_channels),
-            nn.GELU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
-            nn.BatchNorm2d(out_channels)
-        )
-
-        self.process2 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
-            nn.BatchNorm2d(out_channels),
-            nn.GELU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
-            nn.BatchNorm2d(out_channels)
-        )    
+        self.double_conv1 = DoubleConv(in_channels, out_channels)
+        self.double_conv2 = DoubleConv(out_channels, out_channels)
 
         self.maxpool = nn.MaxPool2d(pool_kernal_size)  # reduve the size of the image by kernal_size
 
@@ -79,8 +93,8 @@ class DownSampleBlock(nn.Module):
 
     def forward(self, x, t):
         x = self.maxpool(x) # reduce the image size by a factor of pool_kernal_size
-        x = self.process1(x) # adds the image size by kernal size
-        x = self.process2(x) 
+        x = self.double_conv1(x) # adds the image size by kernal size
+        x = self.double_conv2(x) 
         time_emb = self.emb_layer(t)[:, :, None, None] # equalling to unsqueeze(1).unsqueeze(2)
         return x + time_emb
     
@@ -89,8 +103,8 @@ class UpSampleBlock(nn.Module):
         super().__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        self.conv1 = DoubleConv(in_channels * 2, in_channels, residule=True)
-        self.conv2 = DoubleConv(in_channels, out_channels)
+        self.conv1 = DoubleConv(in_channels * 2, in_channels * 2, residule=True)
+        self.conv2 = DoubleConv(in_channels * 2, out_channels)
         
         self.emb_layer = nn.Sequential(
             nn.SiLU(),
@@ -102,8 +116,9 @@ class UpSampleBlock(nn.Module):
 
     def forward(self, x, skip_x, t):
         x = self.up(x)
+        # print(f"x: {x.shape}, skip_x: {skip_x.shape}")
         x = torch.cat([skip_x, x], dim=1) # is this also residule concat?
-        x = self.conv1(x, residule=skip_x)
+        x = self.conv1(x)
         x = self.conv2(x)
         emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
         return x + emb
@@ -117,20 +132,20 @@ class UNet(nn.Module):
 
         self.DoubleConv = DoubleConv(in_channels, out_channels)
 
-        self.down1 = DownSampleBlock(out_channels, out_channels, time_dim=time_dim)
-        self.sa1 = SelfAttentionBlock(out_channels)
-        self.down2 = DownSampleBlock(out_channels, out_channels, time_dim=time_dim)
-        self.sa2 = SelfAttentionBlock(out_channels)
-        self.down3 = DownSampleBlock(out_channels, out_channels, time_dim=time_dim)
-        self.sa3 = SelfAttentionBlock(out_channels)
+        self.down1 = DownSampleBlock(out_channels, out_channels * 2, time_dim=time_dim)
+        self.sa1 = SelfAttentionBlock(out_channels * 2)
+        self.down2 = DownSampleBlock(out_channels * 2, out_channels * 4, time_dim=time_dim)
+        self.sa2 = SelfAttentionBlock(out_channels * 4)
+        self.down3 = DownSampleBlock(out_channels * 4, out_channels * 4, time_dim=time_dim)
+        self.sa3 = SelfAttentionBlock(out_channels * 4)
 
-        self.mid_conv1 = DoubleConv(out_channels, out_channels)
-        self.mid_conv2 = DoubleConv(out_channels, out_channels)
-        self.mid_conv3 = DoubleConv(out_channels, out_channels)
+        self.mid_conv1 = DoubleConv(out_channels * 4, out_channels * 8)
+        self.mid_conv2 = DoubleConv(out_channels * 8, out_channels * 8)
+        self.mid_conv3 = DoubleConv(out_channels * 8, out_channels * 4)
 
-        self.up1 = UpSampleBlock(out_channels, out_channels, time_dim=time_dim)
-        self.sa4 = SelfAttentionBlock(out_channels)
-        self.up2 = UpSampleBlock(out_channels, out_channels, time_dim=time_dim)
+        self.up1 = UpSampleBlock(out_channels * 4, out_channels * 2, time_dim=time_dim)
+        self.sa4 = SelfAttentionBlock(out_channels * 2)
+        self.up2 = UpSampleBlock(out_channels * 2, out_channels, time_dim=time_dim)
         self.sa5 = SelfAttentionBlock(out_channels)
         self.up3 = UpSampleBlock(out_channels, out_channels, time_dim=time_dim)
         self.sa6 = SelfAttentionBlock(out_channels)
@@ -145,9 +160,15 @@ class UNet(nn.Module):
             ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
         ) # of shape (channels // 2)
 
-        pos_enc_a = torch.sin(t.repeat(1, channels // 2 // t.shape[0]) * inv_freq) # shape (1, channels // 2)
-        pos_enc_b = torch.cos(t.repeat(1, channels // 2 // t.shape[0]) * inv_freq) # shape (1, channels // 2)
-        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1) # shape (1, channels)
+        # Reshape t to (batch_size, 1) for proper broadcasting
+        t = t.unsqueeze(1).type(torch.float)  # shape (batch_size, 1)
+        
+        # Repeat along the second dimension
+        t = t.repeat(1, channels // 2)  # shape (batch_size, channels // 2)
+        
+        pos_enc_a = torch.sin(t * inv_freq)  # shape (batch_size, channels // 2)
+        pos_enc_b = torch.cos(t * inv_freq)  # shape (batch_size, channels // 2)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=1)  # shape (batch_size, channels)
         return pos_enc
 
     def forward(self, x, t):
@@ -159,12 +180,14 @@ class UNet(nn.Module):
         x2 = self.sa1(x2)
         x3 = self.down2(x2, t) # shape (batch_size, out_channels, img_size/4, img_size/4)
         x3 = self.sa2(x3)
+        # print(f"x3: {x3.shape}")
         x4 = self.down3(x3, t) # shape (batch_size, out_channels, img_size/8, img_size/8)
         x4 = self.sa3(x4)
 
         x4 = self.mid_conv1(x4)
         x4 = self.mid_conv2(x4)
         x4 = self.mid_conv3(x4)
+        # print(x4.shape)
 
         x = self.up1(x4, x3, t) # shape (batch_size, out_channels, img_size/4, img_size/4)
         x = self.sa4(x)
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 import torch.nn as nn
 from torch import optim
@@ -9,11 +10,12 @@ from model.module import UNet
 from model.diffusion import Diffusion
 from utils import save_images
 
-def train():
+def train(args, generate=True):
     # Define the transform (convert to tensor, normalize, etc.)
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.RandomResizedCrop(64, scale=(0.8, 1.0)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     """
@@ -23,22 +25,22 @@ def train():
     Classes: ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
     """
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
-    device = "cuda:2"
-    epochs = 5
-
-    model = UNet(in_channels=3, out_channels=32, time_dim=256, device=device).to(device)
-    diffusion = Diffusion(noise_steps=1000, img_size=32, device=device)
+    device = args.device
+    print(f"Device: {device}")
+    model = UNet(in_channels=3, out_channels=64, time_dim=args.time_dim, device=args.device).to(args.device)
+    diffusion = Diffusion(noise_steps=args.noise_steps, img_size=args.img_size, device=args.device)
     mse = nn.MSELoss()
-    optimizer = optim.AdamW(model.parameters(), lr=0.01)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
 
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
+        model.train()
         total_loss = 0.0
         for i, data in enumerate(trainloader):
             inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-            t = diffusion.sample_timesteps(inputs.shape[0])
+            inputs, labels = inputs.to(args.device), labels.to(args.device)
+            t = diffusion.sample_timesteps(inputs.shape[0]) # sample timesteps for each batch
             x_t, noise = diffusion.noise_images(inputs, t)
 
             predicted_noise = model(x_t, t)
@@ -46,29 +48,47 @@ def train():
 
             optimizer.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             total_loss += loss.item()
             if i % 200 == 199:
-                print(f"Epoch: {epoch}, Batch: {i}, Loss: {(total_loss / 200):.3f}")
+                print(f"Epoch: {epoch}, Batch: {i + 1}, Loss: {(total_loss / 200):.3f}")
                 total_loss = 0.0
 
-    # Save the model
-    os.makedirs("saved_models", exist_ok=True)
-    torch.save(model.state_dict(), "saved_models/diffusion.pth")
+        # Save the model for every 10 epochs
+        if epoch % 10 == 9:
+            torch.save(model.state_dict(), f"saved_models/checkpoint@epoch{epoch + 1}.pth")
+            if generate:
+                model.eval()
+                images = diffusion.sample(model, args.sample_num)
+                save_images(images, "images", f"checkpoint@epoch{epoch + 1}_generated.png")
 
-def generate():
-    device = "cuda:2"
-    model = UNet(in_channels=3, out_channels=32, time_dim=256, device=device).to(device)
-    model.load_state_dict(torch.load("saved_models/diffusion.pth"))
+
+def generate(args, device="cuda"):
+    model_name = "checkpoint@epoch49"
+    device = device
+    model = UNet(in_channels=3, out_channels=64, time_dim=256, device=device).to(device)
+    model.load_state_dict(torch.load(f"saved_models/{model_name}.pth"))
     model.eval()
 
-    diffusion = Diffusion(noise_steps=1000, img_size=32, device=device)
+    diffusion = Diffusion(noise_steps=args.noise_steps, img_size=64, device=device)
 
     n = 16 # must be exponent of 2
     images = diffusion.sample(model, n)
-    save_images(images, "images", "generated_image.png")
+    save_images(images, "images", f"{model_name}_generated.png")
 
 if __name__ == "__main__":
-    train()
-    generate()
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.time_dim = 256
+    args.img_size = 64
+    args.noise_steps = 1000
+    args.device = "cuda:2"
+    args.sample_num = 16
+
+    args.lr = 3e-4
+    args.batch_size = 16
+    args.epochs = 100
+    train(args, True)
